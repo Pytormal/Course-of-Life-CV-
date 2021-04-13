0d011feddc4a97b2e198fc9d920909ff15835aad
Alex Andrew Mallory <59344593+Pytormal@users.noreply.github.com>
2021-04-12 18:25:48 -0700

    Update Skills.js

--- cv/src/components/Skills.js
+++ cv/src/components/Skills.js
@@ -8,7 +8,7 @@ const Skills = () => {
                 <p>
                     Designed many apps for homework and web pages.
                      <li>
-                        created this basic CV and other personal projects as well</li>
+                        Created this basic CV and other personal projects as well</li>
                     <li>learned HTML, CSS, LESS, JSX, React, more skills to learn</li>
                 </p>
                 <p>
@@ -18,12 +18,12 @@ const Skills = () => {
                 </p>
                 <p>
                     Lots of Experience in working with lots of Diverse population
-                     <li>working with Chinese tour groups that were staing at Super 8 heading to Yellow Stone park</li>
+                     <li>Working with Chinese tour groups that were staing at Super 8 heading to Yellow Stone park</li>
                 </p>
                 <p>
-                    lots of Eperience cleaning
+                    Lots of Eperience cleaning
                       <li>
-                        woking at Deseret Industries cleaning bathrooms, and sales floor (if needed) and water fountains
+                        Working at Deseret Industries cleaning bathrooms, and sales floor (if needed) and water fountains
                       </li>
                     <li>Cleaned Apartments and became a Team Lead for a group multiple times
                     </li>
@@ -31,16 +31,16 @@ const Skills = () => {
                 <p>
                     Customer Service Experience of at least 6 years
                       <li>
-                        much prefer working with guests or customers in person rather than in a warehouse or on phone calls
+                        Much prefer working with guests or customers in person rather than in a warehouse or on phone calls
                       </li>
                 </p>
                 <p>
                     Leadership Roles
                      <li>
-                        became a Team Lead for groups when working for Nauvoo House apartments.
+                        Became a Team Lead for groups when working for Nauvoo House apartments.
                      </li>
                     <li>
-                        led many teams and a Guild in some video games
+                        Led many teams and a Guild in some video games
                     </li>
                 </p>
 
